9d6895823e539684b1ef68501ab2e607548b5893
itsmeyashraj <iamyashraj@proton.me>
2022-12-30 03:41:30 +0530

    modified:   home/yashraj/modules/desktop/picom.nix

--- home/yashraj/modules/desktop/picom.nix
+++ home/yashraj/modules/desktop/picom.nix
@@ -14,16 +14,46 @@
         "window_type = 'tooltip'"
         "window_type = 'panel'"
         "window_type = 'popup_menu'"
+        "class_g = 'firefox' && window_type = 'utility'"
       ];
 
       shadow = false;
 
       fading = false;
 
-      blur = false;
+      # blur = false;
 
       experimental-backends = true;
       backend = "glx";
+
+      opacity-rule = [
+        "90:class_g    = 'XTerm'"
+        "90:class_g     = 'Alacritty'"
+        "100:class_i    = 'tray'"
+        "100:class_g =   'Code'"
+      ];
+
+      blur = {
+        method = "kawase";
+        strength = 7;
+        # deviation = 1.0;
+        # kernel = "11x11gaussian";
+        background = false;
+        background-frame = false;
+        background-fixed = false;
+        kern = "3x3box";
+      };
+
+      blur-background-exclude = [
+        "window_type = 'dock'"
+        "window_type = 'desktop'"
+        "_GTK_FRAME_EXTENTS@:c"
+        "class_i = 'tray'"
+        "window_type = 'tooltip'"
+        "window_type = 'panel'"
+        "window_type = 'popup_menu'"
+        "class_g = 'firefox' && window_type = 'utility'"
+      ];
     };
   };
 }
