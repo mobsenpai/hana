753430a758f054464b58305ef03e33906aa85f6f
itsmeyashraj <iamyashraj@proton.me>
2022-12-30 18:44:44 +0530

    modified:   home/yashraj/default.nix

--- home/yashraj/default.nix
+++ home/yashraj/default.nix
@@ -12,7 +12,6 @@
     ./modules/colorschemes
     ./modules/shell
     ./modules/desktop/windowManagers/awesome
-    ./modules/programs/alacritty
     ./modules/programs/kitty.nix
     # ./modules/programs/mpd.nix
     # ./modules/programs/neovim.nix
