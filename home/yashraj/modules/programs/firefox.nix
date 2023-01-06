0863346e99b7055aeb439f44eba6d6fbb42f0ab7
MobSenpai <iamyashraj@proton.me>
2023-01-06 13:23:54 +0530

    modified:   home/yashraj/modules/programs/firefox.nix

--- home/yashraj/modules/programs/firefox.nix
+++ home/yashraj/modules/programs/firefox.nix
@@ -44,10 +44,9 @@
 
         extraConfig = ''
           user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
-          user_pref("full-screen-api.ignore-widgets", true);
-          user_pref("media.ffmpeg.vaapi.enabled", true);
-          user_pref("media.rdd-vpx.enabled", true);
           user_pref("browser.compactmode.show", true);
+          user_pref("browser.uidensity", 1);
+          user_pref("svg.context-properties.content.enabled", true);
         '';
       };
     };
