74e39099dad467ad4526b4bdac53e5602e10dfd0
MobSenpai <github.onstage144@passinbox.com>
2023-10-18 02:30:53 +0530

    modified:   home/yashraj/haru/services/default.nix

--- home/yashraj/haru/services/default.nix
+++ home/yashraj/haru/services/default.nix
@@ -1,7 +1,7 @@
 {
   services = {
-    # TODO: update and uncomment
-    # cliphist.enable = true;
+    # TODO: fix cliphist not working
+    cliphist.enable = true;
     network-manager-applet.enable = true;
   };
 }
