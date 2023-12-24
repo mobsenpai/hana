0df01c2723961799891d8a1fb6ed00cd92c17e86
MobSenpai <work.velocity806@passinbox.com>
2023-12-24 10:44:58 +0530

    formatting

--- home/yashraj/shared/optional/programs/firefox.nix
+++ home/yashraj/shared/optional/programs/firefox.nix
@@ -2,28 +2,28 @@
   programs.firefox = {
     enable = true;
     policies = {
-        DisableFirefoxAccounts = true;
-        DisableFirefoxStudies = true;
-        DisablePocket = true;
-        DisableTelemetry = true;
-        DNSOverHTTPS = {
-          Enabled = true;
-          ProviderURL = "https://dns.quad9.net/dns-query";
-        };
-        DontCheckDefaultBrowser = true;
-        EnableTrackingProtection.Value = true;
-        Homepage.StartPage = "previous-session";
-        NoDefaultBookmarks = true;
-        OfferToSaveLogins = false;
-        PasswordManagerEnabled = false;
+      DisableFirefoxAccounts = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DNSOverHTTPS = {
+        Enabled = true;
+        ProviderURL = "https://dns.quad9.net/dns-query";
       };
+      DontCheckDefaultBrowser = true;
+      EnableTrackingProtection.Value = true;
+      Homepage.StartPage = "previous-session";
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      PasswordManagerEnabled = false;
+    };
 
     profiles.yashraj = {
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-          proton-pass
-          tree-style-tab
-          ublock-origin
-        ];
+        proton-pass
+        tree-style-tab
+        ublock-origin
+      ];
 
       search.default = "DuckDuckGo";
       settings = {
