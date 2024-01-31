290daf4f11b0745e5b975d0ac50bd87a9744e22a
mobsenpai <work.velocity806@passinbox.com>
2024-02-01 02:18:36 +0530

    modified:   flake.lock 	modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,6 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    nix-colors.url = "github:misterio77/nix-colors";
     nur.url = "github:nix-community/NUR";
 
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
