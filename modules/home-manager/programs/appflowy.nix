9ec75f25c98242ff7a628991d107448023f4717b
mobsenpai <work.velocity806@passinbox.com>
2024-10-30 15:19:37 +0530

    appflowy: issue link added

--- modules/home-manager/programs/appflowy.nix
+++ modules/home-manager/programs/appflowy.nix
@@ -13,6 +13,9 @@ in
       appflowy
     ];
 
+    # Appflowy browser redirect
+    # https://github.com/NixOS/nixpkgs/pull/338413
+
     desktop.hyprland.binds = let
       appflowy = getExe pkgs.appflowy;
     in [
