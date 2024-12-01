dc686ae8e045d04548d2cdd01c0449430f176392
mobsenpai <work.velocity806@passinbox.com>
2024-12-01 22:41:22 +0530

    hypridle: update config

--- modules/home-manager/services/hypridle.nix
+++ modules/home-manager/services/hypridle.nix
@@ -6,38 +6,39 @@
 }: let
   inherit (lib) mkIf getExe getExe';
   cfg = config.modules.services.hypridle;
-
-  suspendScript = let
-    pw-cli = getExe' pkgs.pipewire "pw-cli";
-    grep = getExe pkgs.ripgrep;
-    systemctl = getExe' pkgs.systemd "systemctl";
-  in
-    pkgs.writeShellScript "suspend-script" ''
-      ${pw-cli} i all 2>&1 | ${grep} running -q
-      # Only suspend if audio isn't running
-      if [ $? == 1 ]; then
-        ${systemctl} suspend
-      fi
-    '';
+  desktopCfg = config.modules.desktop;
+  isHyprland = desktopCfg.windowManager == "Hyprland";
 in
   mkIf cfg.enable
   {
     services.hypridle = {
       enable = true;
       settings = let
-        pidof = getExe' pkgs.procps "pidof";
         loginctl = getExe' pkgs.systemd "loginctl";
         hyprlock = getExe pkgs.hyprlock;
+        hyprctl = getExe' config.wayland.windowManager.hyprland.package "hyprctl";
+        systemctl = getExe' pkgs.systemd "systemctl";
       in {
         general = {
+          after_sleep_cmd = mkIf isHyprland "${hyprctl} dispatch dpms on";
           before_sleep_cmd = "${loginctl} lock-session";
-          lock_cmd = "${pidof} hyprlock || ${hyprlock}";
+          lock_cmd = hyprlock;
         };
 
         listener = [
           {
-            timeout = 1800;
-            on-timeout = suspendScript.outPath;
+            timeout = 300;
+            on-timeout = "${loginctl} lock-session";
+          }
+          (mkIf isHyprland
+            {
+              timeout = 330;
+              on-timeout = "${hyprctl} dispatch dpms off";
+              on-resume = "${hyprctl} dispatch dpms on";
+            })
+          {
+            timeout = 600;
+            on-timeout = "${systemctl} suspend";
           }
         ];
       };
