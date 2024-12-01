9152e4d0fd6f586d666908376b8eafe0abf15686
mobsenpai <work.velocity806@passinbox.com>
2024-12-01 11:13:39 +0530

    skim: fzf -> skim

---
+++ modules/home-manager/shell/skim.nix
@@ -0,0 +1,24 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: let
+  inherit (lib) mkIf getExe;
+  fd = getExe pkgs.fd;
+  bat = getExe pkgs.bat;
+in
+  mkIf config.modules.shell.enable
+  {
+    programs.skim = {
+      enable = true;
+      defaultCommand = "${fd} -H --type f";
+      changeDirWidgetCommand = "${fd} --type d --hidden --exclude .git";
+      fileWidgetCommand = "${fd} --type f --hidden --exclude .git --exclude .cache";
+      fileWidgetOptions = ["--preview '${bat} --style=numbers --color=always --line-range :500 {}'"];
+
+      defaultOptions = [
+        "--height 20%"
+      ];
+    };
+  }
