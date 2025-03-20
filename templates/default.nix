38650130ba4e6929d6e15d42a114fbe329b8e2ed
mobsenpai <work.velocity806@passinbox.com>
2025-03-20 20:30:02 +0530

    oops

--- templates/default.nix
+++ templates/default.nix
@@ -3,4 +3,14 @@
     path = ./minimal-dev-shell;
     description = "Minimal dev shell flake";
   };
+
+  python-dev-shell = {
+    path = ./python-dev-shell;
+    description = "Python dev shell flake";
+  };
+
+  web-dev-shell = {
+    path = ./web-dev-shell;
+    description = "WebDev shell flake";
+  };
 }
