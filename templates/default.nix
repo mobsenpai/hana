3b07f04bfb89ce18d22e62c9f4ca0d31f9e9f557
mobsenpai <work.velocity806@passinbox.com>
2025-04-20 03:28:33 +0530

    oops

--- templates/default.nix
+++ templates/default.nix
@@ -4,13 +4,18 @@
     description = "C dev shell flake";
   };
 
+  js-dev-shell = {
+    path = ./js-dev-shell;
+    description = "Javascript shell flake";
+  };
+
   minimal-dev-shell = {
     path = ./minimal-dev-shell;
     description = "Minimal dev shell flake";
   };
 
   python-dev-shell = {
-    path = ./python-dev-shell;
+    path = ./py-dev-shell;
     description = "Python dev shell flake";
   };
 
